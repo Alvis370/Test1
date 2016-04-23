f53385939bfe0031fd17954a21219cdc9349e9b1
Alvis Li <lige370@gmail.com>
2016-04-22 22:27:13 -0400

    timestamp changed

--- facebook-clone/client/main.js
+++ facebook-clone/client/main.js
@@ -6,5 +6,9 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
-
+Template.categories.helpers({
+    categories: ["Construction", "Manufacturing", "Wholesale", 
+    "Retail Sales", "Finance", "Insurance", "Real Estate", "Health Care",
+     "Education", "Government", "Other"]
+});
 
